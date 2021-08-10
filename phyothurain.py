25c747b188c7e25950aaee7d61f71dc50f418da6
ۥٰ ۥٰ <88605415+phyothurain295646@users.noreply.github.com>
2021-08-11 00:49:46 +0630

    phyothurain.py
    # fb-cracker
    
    [x]THIS IS FOR TERMUX USER ONLY[x]
    
    AUTO FACEBOOK CRACKER
    
    
    <h2>INSTALLATION:</h2>
    
    <p>pkg update</p>
    
    <p>pkg install git</p>
    
    <p>pkg install python</p>
    
    pkg install python2
    
    git clone https://github.com/phyothurain295646/phyothurain.py.git
    
    cd fb-cracker
    
    pip2 install -r requirements.txt
    
    
    <h2>HOW TO USE:</h2>
    
    python fb-phyothurain.py
    
    login your fb account

---
+++ phyothurain.py
@@ -0,0 +1,32 @@
+# fb-cracker
+
+[x]THIS IS FOR TERMUX USER ONLY[x]
+
+AUTO FACEBOOK CRACKER
+
+
+<h2>INSTALLATION:</h2>
+
+<p>pkg update</p>
+
+<p>pkg install git</p>
+
+<p>pkg install python</p>
+
+pkg install python2
+
+git clone https://github.com/phyothurain295646/phyothurain.py.git
+
+cd fb-cracker
+
+pip2 install -r requirements.txt
+
+
+<h2>HOW TO USE:</h2>
+
+python fb-phyothurain.py
+
+login your fb account
+
+
+
